ff96860d1e71c8a216941ca7edc31224c17ef90c
AslinurTolga <aslinurtolga@gmail.com>
2023-01-19 14:55:43 +0300

    feat: data listed on screen with map

--- 2-tours/src/Tours.js
+++ 2-tours/src/Tours.js
@@ -4,6 +4,16 @@ const Tours = ({tours}) => {
   return <section>
     <div className="title">
       <h2>ours tours</h2>
+      <div className="underline">
+
+      </div>
+      <div>
+        {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour}>
+
+        </Tour>
+        })}
+      </div>
     </div>
   </section>;
 };
